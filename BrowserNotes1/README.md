ee460e18165bf21c57409b4c50b272364aa5816f
Carlos <28845529+SrCharlystar@users.noreply.github.com>
2019-02-09 23:51:15 +0100

    Launching version 2.0
    Version 2.0 is multi-note ready. Next version will allow adding them into files.

--- BrowserNotes1/README.md
+++ BrowserNotes1/README.md
@@ -1,8 +1,9 @@
 
-# BrowserNotes 2.0
-Simple, minimal and small HTML document to save your passwords or notes AES-encrypted in your web browser or device.
+# BrowserNotes
 
-File format of BrowserNotes2 is ready for multiple notes within same file, device or bookmark. Also, encryption is now applied once to the file (before, title and content were encrypted separately, making script slower without giving any security improvement)
+**DEPRECATED VERSION: Please consider moving to BrowserNotes2 for multinote and better performance**
+
+Simple, minimal and small HTML document to save your passwords or notes AES-encrypted in your web browser or device.
 
 BrowserNotes is designed to be portable, so that a single file contains everything necessary to run correctly. Also, it is designed to be used without internet connection, including all the necessary libraries.
 
@@ -11,7 +12,7 @@ You can save your note into:
 
  1. **WebStorage**: Save your encrypted note into your browser clicking "Save to WebStorage".
  2. **File**: If you need to save multiple notes, you can save them in your device in a JSON based file, wich stores your note encrypted.
- 3. **URL**: Not recommended. Generates a URL to save your note in your bookmarks. It is not reccomended because browsers can not handle URLs with unlimited chars. Also, you need to re-bookmark your note every time you edit it.
+ 3. **URL**: Not recommended. Generates a URL to save your note in your bookmarks. It is not reccomended because you need to re-bookmark your note every time you edit it.
 
 ### Also included
 
--- script/service.js
+++ script/service.js
@@ -3,275 +3,281 @@ Encriptador v2.0.1
 https://github.com/SrCharlystar
 (c) 2018-2019 SrCharlystar. All rights reserved.
 */
-class Encriptador{
-	startup(){
-		//Language phrases and variable initialization
-		this.encryptedTitle;
-		this.encryptedContent;
-		this.pwd;
-		this.autosave = false;
-		this.lang;
-		this.setLanguage("en_US");
-		//Check for possible notes in the URL. If set, load saved note and ask for password.
-		if(window.location.search){
-			this.encryptedTitle = this.getParameterByName("t");
-			this.encryptedContent = this.getParameterByName("d");
-			this.pwdPrompt();
-		}
-	}
+var jsonFile; //Decrypted JSON file
+var index; //Index of note in JSON file
+var pwd;
+var lang;
 
-	/**
-	 * Asks for passwd
-	 */
-	pwdPrompt(){
-		//Ask for password and decrypt content
-		this.pwd = prompt(this.lang.pwd_ask);
-		this.decrypt();
-		var tmp = this.getText();
-		//Check for incorrect password and prompt again
-		while(tmp == ""){
-			this.pwd = prompt(this.lang.pwd_ask_retry);
-			this.decrypt();
-			tmp = this.getText();
-		}
-	}
-
-	/**
-	 * Encrypts note
-	 * If password was not set, ask user for it.
-	 */
-	encrypt(){
-		if(!this.pwd)
-			this.chPwd();
-		this.encryptedTitle = CryptoJS.AES.encrypt(this.getTitle(), this.pwd);
-		this.encryptedContent = CryptoJS.AES.encrypt(this.getText(), this.pwd);
-	}
-
-	/**
-	 * Decrypts note and updates document.
-	 * If password is incorrect (exception catched) prompt it again
-	 */
-	decrypt(){
-		try {
-			document.getElementById("title").innerText = CryptoJS.AES.decrypt(this.encryptedTitle, this.pwd).toString(CryptoJS.enc.Utf8);
-			document.getElementById("main").innerText = CryptoJS.AES.decrypt(this.encryptedContent, this.pwd).toString(CryptoJS.enc.Utf8);
-		} catch (e) {
-			this.pwdPrompt();
-			this.decrypt();
-		}
-	}
-
-	/**
-	 * Get note title
-	 */
-	getTitle(){
-		return document.getElementById("title").innerText;
-	}
-
-	/**
-	 * Get note content
-	 */
-	getText(){
-		return document.getElementById("main").innerText;
-	}
-
-	/**
-	 * Change/set password
-	 */
-	chPwd(){
-		var pwd1 = prompt(this.lang.pwd_change);
-		var pwd2 = prompt(this.lang.pwd_change_confirm);
-		if(pwd1 != pwd2){
-			this.showInfo(this.lang.pwd_change_error);
-			return;
-		}
-		this.pwd = pwd1;
-		this.encrypt();
-		this.showInfo(this.lang.pwd_change_correct);
-		this.setLink();
-	}
-
-	/**
-	 * Update document language
-	 */
-	setLanguage(locale){
-		var oldLang = this.lang; //Used to check if title/content changed
-		//Set dialog phrases
-		switch(locale){
-			case "en_US":
-				this.lang = {
-				  "title": "Encrypted note",
-				  "note_title": "Untitled note",
-				  "note_content": "Hello, World!",
-				  "save_to_file": "Save to file",
-				  "load_file": "Load file",
-				  "change_password": "Change password",
-				  "generate_link": "Generate link",
-				  "toggle_autosave": "Enable/Disable auto-save",
-				  "pwd_ask": "Please type your password.",
-				  "pwd_ask_retry": "Password is not correct!\nPlease insert your password.",
-				  "pwd_change": "Please type your new password.",
-				  "pwd_change_confirm": "Please confirm your password.",
-				  "pwd_change_error": "Passwords do not match so password was not changed.",
-				  "pwd_change_correct": "Password changed",
-				  "get_parameters_set": "Done. Remember to save page to bookmarks!",
-				  "encrypted_note": "Encrypted Note",
-				  "auto_encode_toggle": "Automatic link generation was set to ",
-				  "saved": "Saved.",
-				  "file_loaded": "File loaded successfully.",
-				  "save_storage": "Save to WebStorage",
-				  "load_storage": "Load from WebStorage"
-				}
-				break;
-			case "es_ES":
-				this.lang = {
-				  "title": "Nota encriptada",
-				  "note_title": "Nota sin título",
-				  "note_content": "¡Hola mundo!",
-				  "save_to_file": "Guardar a archivo",
-				  "load_file": "Cargar archivo",
-				  "change_password": "Cambiar contraseña",
-				  "generate_link": "Generar enlace",
-				  "toggle_autosave": "Activar/Desactivar autoguardado",
-				  "pwd_ask": "Por favor, introduce la contraseña.",
-				  "pwd_ask_retry": "La contraseña no es correcta.\nPor favor, introduce la contraseña.",
-				  "pwd_change": "Escribe tu nueva contraseña",
-				  "pwd_change_confirm": "Confirma tu contraseña.",
-				  "pwd_change_error": "Las contraseñas no coinciden por lo que tu contraseña no ha sido cambiada.",
-				  "pwd_change_correct": "Contraseña modificada con éxito.",
-				  "get_parameters_set": "Hecho. Recuerda guardar la página en marcadores o descargar el fichero.",
-				  "encrypted_note": "Nota encriptada",
-				  "auto_encode_toggle": "La generacion automatica de enlaces ha sido cambiada a ",
-				  "saved": "Guardado.",
-				  "file_loaded": "Archivo cargado con éxito.",
-				  "save_storage": "Guardar WebStorage",
-				  "load_storage": "Leer WebStorage"
-				}
-				break;
-			default:
-				console.error("No language set");
-		}
-		//Set button phrases
-		document.title = this.lang.title;
-		document.getElementById("save").textContent = this.lang.save_to_file;
-		document.getElementById("load").textContent = this.lang.load_file;
-		document.getElementById("chpwd").textContent = this.lang.change_password;
-		document.getElementById("glinkbtn").textContent = this.lang.generate_link;
-		document.getElementById("tglautosavebtn").textContent = this.lang.toggle_autosave;
-		document.getElementById("savestorage").textContent = this.lang.save_storage;
-		document.getElementById("loadstorage").textContent = this.lang.load_storage;
-		if(!oldLang || oldLang.note_title == this.getTitle())
-			document.getElementById("title").innerText = this.lang.note_title;
-		if(!oldLang || oldLang.note_content == this.getText())
-			document.getElementById("main").innerText = this.lang.note_content;
-	}
-
-	/**
-	 * Show info to user
-	 */
-	showInfo(text){
-		if(text != "")
-			document.getElementById("info").innerHTML = text + " | ";
-	}
-
-	/**
-	 * Saves and reports user
-	 */
-	saveToLink(){
-		this.setLink();
-		this.showInfo(this.lang.get_parameters_set);
-	}
-
-	/**
-	 * Updates address' bar link
-	 * Called only when a save WITHOUT REPORTING to the user is needed.
-	 */
-	setLink(){
-		this.encrypt();
-		window.history.pushState('', this.lang.encrypted_note, '?t=' + this.encryptedTitle + '&d=' + this.encryptedContent);
-	}
-
-	/**
-	 * Toggle autosave and show info to the user
-	 */
-	toggleAutoSave(){
-		this.autosave = !this.autosave;
-		this.showInfo(this.lang.auto_encode_toggle + this.autosave);
-	}
-
-	/**
-	 * Fired when a key is pressed. Saves changes if autosave is enabled
-	 */
-	trySave(){
-		if(this.autosave)
-			this.setLink();
-	}
-
-	/**
-	 * Saves note to a file
-	 */
-	saveFile(){
-		//Save changes
-		this.encrypt();
-		//JSON string creation
-		var obj = new Object();
-		obj.title = this.encryptedTitle.toString();
-		obj.content = this.encryptedContent.toString();
-		var jsonString = JSON.stringify(obj, null, 4);
-		this.download(this.getTitle() + "-encrypted.AESjson", jsonString)
-		this.showInfo(this.lang.saved);
-	}
+/**
+ * Language phrases and variable initialization
+ * Also, check for possible notes in the URL. If set, load saved note and ask for password.
+ */
+function startup(){
+	index = 0;
+	jsonFile = new Object();
+	jsonFile.version = 1;
+	jsonFile.notes = new Array();
+	jsonFile.notes[index] = new Object();
+	setLanguage("en_US");
 	
-	saveStorage(){
-		//Save changes
-		this.encrypt();
-		window.localStorage.title = this.encryptedTitle;
-		window.localStorage.content = this.encryptedContent;
-		this.showInfo(this.lang.saved);
-	}
-	
-	loadStorage(){
-		this.encryptedTitle = window.localStorage.title;
-		this.encryptedContent = window.localStorage.content;
-		//Desencriptamos
-		this.decrypt();
-		//Informamos
-		this.showInfo(this.lang.file_loaded);
-	}
-	
-	loadFile(o){
-		var fr = new FileReader();
-		fr.onloadend = function(e){
-			var tmp = JSON.parse(e.target.result);
-			document.getElementById("title").innerText = "";
-			document.getElementById("main").innerText = "";
-			enc.encryptedTitle = tmp.title;
-			enc.encryptedContent = tmp.content;
-			enc.pwdPrompt();
-			enc.decrypt();
-			enc.showInfo(enc.lang.file_loaded);
-		};
-		fr.readAsText(o.files[0]);
-	}
-
-	getParameterByName(name) {
-		var url = window.location.href;
-		name = name.replace(/[\[\]]/g, '\\$&');
-		var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-			results = regex.exec(url);
-		if (!results) return null;
-		if (!results[2]) return '';
-		return decodeURIComponent(results[2]);
-	}
-
-	download(filename, text) {
-		var element = document.createElement('a');
-		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-		element.setAttribute('download', filename);
-		element.style.display = 'none';
-		document.body.appendChild(element);
-		element.click();
-		document.body.removeChild(element);
+	if(window.location.search){
+		loadURL();
 	}
 }
-var enc = new Encriptador();
+
+/**
+ * Asks for password and saves it into pwd variable.
+ */
+function pwdPrompt(isRetry){
+	if(isRetry)
+		pwd = prompt(lang.pwd_ask_retry);
+	else
+		pwd = prompt(lang.pwd_ask);
+}
+
+/**
+ * Change/set password
+ */
+function chPwd(){
+	var pwd1 = prompt(lang.pwd_change);
+	var pwd2 = prompt(lang.pwd_change_confirm);
+	if(pwd1 != pwd2){
+		showInfo(lang.pwd_change_error);
+		return;
+	}
+	pwd = pwd1;
+	showInfo(this.lang.pwd_change_correct);
+}
+
+/**
+ * Receives unencrypted note and return it encrypted.
+ * If password was not set, ask user for it.
+ */
+function encrypt(unencryptedObj){
+	if(!pwd)
+		chPwd();
+	reflectChangesOnJson(); //Make sure json file is updated
+	var jsonString = JSON.stringify(unencryptedObj, null, 4);
+	return CryptoJS.AES.encrypt(jsonString, pwd);
+}
+
+/**
+ * Receives encrypted note and returns it decrypted.
+ * If password is incorrect (exception catched or empty string) prompt it again.
+ */
+function decrypt(encryptedJson){
+	try {
+		var result = CryptoJS.AES.decrypt(encryptedJson, pwd).toString(CryptoJS.enc.Utf8);
+		if(result == "")
+			throw "Not valid";
+		return JSON.parse(result);
+	} catch (e) {
+		pwdPrompt(true);
+		return decrypt(encryptedJson);
+	}
+}
+
+/**
+ * Get current note title
+ */
+function getTitle(){
+	return document.getElementById("title").innerText;
+}
+
+/**
+ * Get current note content
+ */
+function getText(){
+	return document.getElementById("main").innerText;
+}
+
+/**
+ * Update document language
+ */
+function setLanguage(locale){
+	var oldLang = lang; //Used to check if title/content changed
+	//Set dialog phrases
+	switch(locale){
+		case "en_US":
+			lang = {
+			  "title": "Encrypted note",
+			  "note_title": "Untitled note",
+			  "note_content": "Hello, World!",
+			  "save_to_file": "Save to file",
+			  "load_file": "Load file",
+			  "change_password": "Change password",
+			  "generate_link": "Generate link",
+			  "pwd_ask": "Please type your password.",
+			  "pwd_ask_retry": "Password is not correct!\nPlease insert your password.",
+			  "pwd_change": "Please type your new password.",
+			  "pwd_change_confirm": "Please confirm your password.",
+			  "pwd_change_error": "Passwords do not match so password was not changed.",
+			  "pwd_change_correct": "Done. Save your note to finish password change.",
+			  "get_parameters_set": "Done. Remember to save page to bookmarks!",
+			  "encrypted_note": "Encrypted Note",
+			  "auto_encode_toggle": "Automatic link generation was set to ",
+			  "saved": "Saved.",
+			  "file_loaded": "File loaded successfully.",
+			  "save_storage": "Save to WebStorage",
+			  "load_storage": "Load from WebStorage"
+			}
+			break;
+		case "es_ES":
+			lang = {
+			  "title": "Nota encriptada",
+			  "note_title": "Nota sin título",
+			  "note_content": "¡Hola mundo!",
+			  "save_to_file": "Guardar a archivo",
+			  "load_file": "Cargar archivo",
+			  "change_password": "Cambiar contraseña",
+			  "generate_link": "Generar enlace",
+			  "pwd_ask": "Por favor, introduce la contraseña.",
+			  "pwd_ask_retry": "La contraseña no es correcta.\nPor favor, introduce la contraseña.",
+			  "pwd_change": "Escribe tu nueva contraseña",
+			  "pwd_change_confirm": "Confirma tu contraseña.",
+			  "pwd_change_error": "Las contraseñas no coinciden por lo que tu contraseña no ha sido cambiada.",
+			  "pwd_change_correct": "Hecho. Guarda la nota para guardar la nueva contraseña.",
+			  "get_parameters_set": "Hecho. Recuerda guardar la página en marcadores o descargar el fichero.",
+			  "encrypted_note": "Nota encriptada",
+			  "auto_encode_toggle": "La generacion automatica de enlaces ha sido cambiada a ",
+			  "saved": "Guardado.",
+			  "file_loaded": "Archivo cargado con éxito.",
+			  "save_storage": "Guardar WebStorage",
+			  "load_storage": "Leer WebStorage"
+			}
+			break;
+		default:
+			console.error("No language set");
+	}
+	//Set button phrases
+	document.title = lang.title;
+	document.getElementById("save").textContent = lang.save_to_file;
+	document.getElementById("load").textContent = lang.load_file;
+	document.getElementById("chpwd").textContent = lang.change_password;
+	document.getElementById("glinkbtn").textContent = lang.generate_link;
+	document.getElementById("savestorage").textContent = lang.save_storage;
+	document.getElementById("loadstorage").textContent = lang.load_storage;
+	if(!oldLang || oldLang.note_title == getTitle())
+		document.getElementById("title").innerText = lang.note_title;
+	if(!oldLang || oldLang.note_content == getText())
+		document.getElementById("main").innerText = lang.note_content;
+}
+
+/**
+ * Show info to user
+ */
+function showInfo(text){
+	if(text != "")
+		document.getElementById("info").innerHTML = text + " | ";
+}
+
+/**
+ * Fired when a key is pressed. Reflects text changes to JSON file
+ */
+function reflectChangesOnJson(){
+	jsonFile.notes[index].title = getTitle();
+	jsonFile.notes[index].content = getText();
+}
+
+/**
+ * Reflects JSON file in screen.
+ */
+function updateVisibleNote(){
+	document.getElementById("title").innerText = jsonFile.notes[index].title;
+	document.getElementById("main").innerText = jsonFile.notes[index].content;
+}
+
+//DOWN HERE SAVING FUNCTIONS
+
+/**
+ * Saves note into WebStorage
+ */
+function saveStorage(){
+	var encryptedNoteResult = encrypt(jsonFile);
+	
+	window.localStorage.encryptedJsonString = encryptedNoteResult;
+	showInfo(lang.saved);
+}
+
+/**
+ * Saves note to a file
+ */
+function saveFile(){
+	var encryptedNoteResult = encrypt(jsonFile);
+	
+	download(getTitle() + "-encrypted.AESjson", encryptedNoteResult)
+	showInfo(lang.saved);
+}
+
+/**
+ * Saves to URL and reports user
+ */
+function saveToLink(){
+	setLink();
+	showInfo(lang.get_parameters_set);
+}
+
+/**
+ * Updates address bar link
+ * Called only when a save WITHOUT REPORTING to the user is needed.
+ */
+function setLink(){
+	var encryptedNoteResult = encrypt(jsonFile);
+	
+	window.history.pushState('', lang.encrypted_note, '?data=' + encryptedNoteResult);
+}
+
+//DOWN HERE LOADING FUNCTIONS
+	
+/**
+ * Loads note from WebStorage
+ */
+function loadStorage(){
+	jsonFile = decrypt(window.localStorage.encryptedJsonString);
+	updateVisibleNote();
+	showInfo(lang.file_loaded);
+}
+
+/**
+ * Loads from file
+ */
+function loadFile(o){
+	var fr = new FileReader();
+	fr.onloadend = function(e){
+		jsonFile = decrypt(e.target.result)
+		updateVisibleNote();
+		showInfo(lang.file_loaded);
+	};
+	fr.readAsText(o.files[0]);
+}
+
+/**
+ *
+ */
+function loadURL(){
+	var encJsonFile = getParameterByName("data");
+	jsonFile = decrypt(encJsonFile);
+	updateVisibleNote();
+}
+
+//EXTRA FUNCTIONS
+
+function getParameterByName(name) {
+	var url = window.location.href;
+	name = name.replace(/[\[\]]/g, '\\$&');
+	var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2]);
+}
+
+function download(filename, text) {
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('download', filename);
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
